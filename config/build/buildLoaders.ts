03ecada15cfc4887815ecf34781ad93803ec5acd
tanovik <taisianovikova@gmail.com>
2023-07-31 22:40:54 +0300

    config added

---
+++ config/build/buildLoaders.ts
@@ -0,0 +1,36 @@
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import webpack from "webpack";
+import { BuildOptions } from "./types/config";
+
+export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
+ 
+  const cssLoaders = {
+    test: /\.s[ac]ss$/i,
+    use: [
+     isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+     {
+      loader: "css-loader",
+      options: {
+        modules: {
+          auto: (resPath:string) => Boolean( resPath.includes('.module.')),
+          localIdentName: isDev 
+          ? '[path][name]__[local]--[hash:base64:8]' 
+          : '[hash:base64:8]'
+        },
+        }
+    },
+      "sass-loader",
+    ],
+  };
+
+  const typescriptLoader = {
+    test: /\.tsx?$/,
+    use: "ts-loader",
+    exclude: /node_modules/,
+  };
+
+  return [
+    typescriptLoader,
+    cssLoaders,
+  ];
+}
