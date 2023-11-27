a54e1feb285b4f1ff0f6ce89b01a3d74f4b72f3c
tanovik <taisianovikova@gmail.com>
2023-11-27 16:48:44 +0300

    navigate import fixed

--- src/app/providers/StoreProvider/ui/StoreProvider.tsx
+++ src/app/providers/StoreProvider/ui/StoreProvider.tsx
@@ -2,7 +2,6 @@ import { Provider } from 'react-redux'
 import { createReduxStore } from '../config/store'
 import { type StateSchema } from '../config/StateSchema'
 import { type ReducersMapObject } from '@reduxjs/toolkit'
-import { useNavigate } from 'react-router-dom'
 
 interface StoreProviderProps {
     children?: React.ReactNode
