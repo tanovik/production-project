5ec9c2cd96bca158cba446572d71bda281c10091
tanovik <taisianovikova@gmail.com>
2023-12-26 21:07:09 +0300

    fixed storybook after refactoring, update screenshots

--- config/storybook/preview.tsx
+++ config/storybook/preview.tsx
@@ -15,7 +15,8 @@ const preview: Preview = {
                 color: /(background|color)$/i,
                 date: /Date$/
             }
-        }
+        },
+        layout: 'fullscreen'
     }
 
 }
