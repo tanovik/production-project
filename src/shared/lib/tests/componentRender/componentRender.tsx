5d184e04aac2a6944784bf9aaaeb89f9892568d4
tanovik <taisianovikova@gmail.com>
2024-01-18 13:49:34 +0300

    added test for isolated component with cypress

--- src/shared/lib/tests/componentRender/componentRender.tsx
+++ src/shared/lib/tests/componentRender/componentRender.tsx
@@ -1,28 +1,51 @@
 import { type ReducersMapObject } from '@reduxjs/toolkit'
 import { type RenderResult, render } from '@testing-library/react'
 import { type StateSchema, StoreProvider } from '@/app/providers/StoreProvider'
-import { type ReactNode } from 'react'
+import { type ReactElement, type ReactNode } from 'react'
 import { I18nextProvider } from 'react-i18next'
 import { MemoryRouter } from 'react-router-dom'
 import i18nForTest from '@/shared/config/i18n/i18nForTest'
+import { Theme } from '@/shared/const/theme'
+// eslint-disable-next-line tanovik-plugin/layer-imports
+import { ThemeProvider } from '@/app/providers/ThemeProvider'
+// eslint-disable-next-line tanovik-plugin/layer-imports
+import '@/app/styles/index.scss'
 
 export interface componentRenderOptions {
     route?: string
     initialState?: DeepPartialType<StateSchema>
     asyncReducers?: DeepPartialType<ReducersMapObject<StateSchema>>
+    theme?: Theme
+}
+interface TestProviderProps {
+    children: ReactNode
+    options?: componentRenderOptions
 }
 
-export function componentRender (component: ReactNode,
-    options: componentRenderOptions = {}): RenderResult {
-    const { route = '/', initialState, asyncReducers } = options
+export function TestProvider (props: TestProviderProps): ReactElement {
+    const { children, options = {} } = props
+    const {
+        route = '/',
+        initialState,
+        asyncReducers,
+        theme = Theme.LIGHT
+    } = options
 
-    return render(
+    return (
         <MemoryRouter initialEntries={[route]}>
             <StoreProvider asyncReducers={asyncReducers} initialState={initialState}>
                 <I18nextProvider i18n={i18nForTest}>
-                    {component}
+                    <ThemeProvider initialTheme={theme}>
+                        <div className={`app ${theme}`}>
+                            {children}
+                        </div>
+                    </ThemeProvider>
                 </I18nextProvider>
             </StoreProvider>
         </MemoryRouter>
     )
+}
+
+export function componentRender (component: ReactNode, options: componentRenderOptions = {}): RenderResult {
+    return render(<TestProvider options={options}>{component}</TestProvider>)
 }
